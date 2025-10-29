cc2cd44799bb9929ddea8a1aa884b4cb9c45aaaa
Dan Wilson <55405712+danwilson282@users.noreply.github.com>
2025-10-29 20:53:53 +0000

    add richtext links (#9)

--- src/sections/textBlock/index.tsx
+++ src/sections/textBlock/index.tsx
@@ -1,9 +1,14 @@
 import { SanityTextBlockSection } from '@/sanity/types/sections/TextBlock';
 import { TextBlock } from '@danwilson282/mkj-component-library';
+import React from 'react';
 type SectionProps = {
   section: SanityTextBlockSection;
 };
 
 export const TextBlockSection: React.FC<SectionProps> = ({ section }) => {
-  return <TextBlock value={section.content}></TextBlock>;
+  return (
+    <TextBlock
+      value={section.content}
+    />
+  );
 };
